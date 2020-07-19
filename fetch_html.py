26931b6e19708004787560603828a04b221e389b
Hesham <heshammeneisi@gmail.com>
2020-07-19 14:48:36 +0700

    feat(fetching): implement a simple cache

--- fetch_html.py
+++ fetch_html.py
@@ -1,12 +1,26 @@
 import boto3
+from time import time
+from copy import deepcopy
 
-s3_client = boto3.client('s3')
+cached_state = None
+cache_timestamp = None
+cache_ttl = 60
 
 
 def fetch_html(bucket, index_key="index.html"):
-    response = s3_client.get_object(Bucket=bucket, Key=index_key)
-    return {
-        "html": response["Body"].read().decode('utf-8'),
-        "headers": response["ResponseMetadata"]["HTTPHeaders"],
-        "status":  response["ResponseMetadata"]["HTTPStatusCode"]
-    }
+    global cached_state
+    global cache_timestamp
+    now = time()
+    if cached_state and now - cache_timestamp < cache_ttl:
+        return deepcopy(cached_state)
+    else:
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(Bucket=bucket, Key=index_key)
+        state = {
+            "html": response["Body"].read().decode('utf-8'),
+            "headers": response["ResponseMetadata"]["HTTPHeaders"],
+            "status":  response["ResponseMetadata"]["HTTPStatusCode"]
+        }
+        cached_state = deepcopy(state)
+        cache_timestamp = now
+        return state
