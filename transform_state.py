a2ef3c1df3e7aa4417cf7fdb3881ca843d5fcf4f
Hesham <heshammeneisi@gmail.com>
2020-07-29 00:50:22 +0700

    fix(transform): correct fallback

--- transform_state.py
+++ transform_state.py
@@ -6,13 +6,18 @@ def transform_state(state, rule):
     for t in rule["transformations"]:
         apply_trans(t, state)
 
- 
+
 def apply_trans(t, state):
-    t_func = getattr(transformers, t["name"])
-
-    if t_func is None:
-        t_func = getattr(_transformers, t["name"])
-
+    t_func = None
+    
+    try:
+        t_func = getattr(transformers, t["name"])
+    except:
+        try:
+            t_func = getattr(_transformers, t["name"])
+        except:
+            pass
+        
     if t_func is None:
         print(f"Unknown transformer {t['name']}")
     else:
