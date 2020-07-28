c4f8eb92e329d839ab0697684a456ac6dff6df4f
Hesham <heshammeneisi@gmail.com>
2020-07-29 01:10:32 +0700

    feat(lambda): allow silent exceptions

--- _config.py
+++ _config.py
@@ -3,3 +3,4 @@
 bucket_name = "ORIGIN_BUCKET_NAME"
 index_key = "index.html"
 html_cache_ttl = 60
+raise_exceptions = False
--- lambda_function.py
+++ lambda_function.py
@@ -4,29 +4,33 @@ from transform_state import transform_state
 from helpers import structure_headers, get_pattern
 from _rules import rules
 from _exclusions import exclusions
-from _config import bucket_name, index_key
+from _config import bucket_name, index_key, raise_exceptions
 
 
 def lambda_handler(event, context):
     request = event["Records"][0]["cf"]["request"]
     
-    for pattern in exclusions:
-        if re.match(pattern, request["uri"]):
-            return request
+    try:
+        for pattern in exclusions:
+            if re.match(pattern, request["uri"]):
+                return request
         
-    for rule in rules: 
-        if not re.match(get_pattern(rule), request["uri"]):
-            continue
-        state = fetch_html(bucket_name, index_key)
-        state['request'] = request
-        state["response_headers"] = {"content-type": "text/html"}
-        status = state["status"]
-        if status>= 200 and status < 400:
-            transform_state(state, rule)
-        return {
-            "status": state["status"],
-            "body": state["html"],
-            "headers": structure_headers(state["response_headers"])
-        }
-        
+        for rule in rules: 
+            if not re.match(get_pattern(rule), request["uri"]):
+                continue
+            state = fetch_html(bucket_name, index_key)
+            state['request'] = request
+            state["response_headers"] = {"content-type": "text/html"}
+            status = state["status"]
+            if status>= 200 and status < 400:
+                transform_state(state, rule)
+            return {
+                "status": state["status"],
+                "body": state["html"],
+                "headers": structure_headers(state["response_headers"])
+            }
+    except Exception as ex:
+        if raise_exceptions:
+            raise ex
+
     return request
